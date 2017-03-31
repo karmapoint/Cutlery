58c14d73a8d239619b7b9ab3226bb983f61485d1
Brent <karmapoint@gmail.com>
2017-03-31 11:11:35 -0700

    push fix to production

--- bundle.js
+++ bundle.js
@@ -60,7 +60,7 @@
 /******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
 /******/
 /******/ 	// __webpack_public_path__
-/******/ 	__webpack_require__.p = "http://localhost:8080/";
+/******/ 	__webpack_require__.p = "https://karmapoint.github.io/Cutlery/";
 /******/
 /******/ 	// Load entry module and return exports
 /******/ 	return __webpack_require__(__webpack_require__.s = 7);
